26bcf0c1a125fce672739dae081ae5824111aab1
Maria Celeste <maria_rr_13@hotmail.com>
2021-06-11 12:05:25 -0400

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { OrderModule } from 'ngx-order-pipe';
+import { ModalModule } from '../_modal';
 
 import { AppComponent } from './app.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { TransferListComponent } from './transfer-list/transfer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferComponent,
+    TransferListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    OrderModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
